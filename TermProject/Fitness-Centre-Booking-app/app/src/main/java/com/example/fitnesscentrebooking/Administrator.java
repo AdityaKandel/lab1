1994d525b4c0e52c0710005b2bede45a2b24a3ad
omaraly1 <oaly042@uottawa.ca>
2021-10-20 21:00:35 -0400

    test

--- TermProject/Fitness-Centre-Booking-app/app/src/main/java/com/example/fitnesscentrebooking/Administrator.java
+++ TermProject/Fitness-Centre-Booking-app/app/src/main/java/com/example/fitnesscentrebooking/Administrator.java
@@ -1,6 +1,7 @@
 package com.example.fitnesscentrebooking;
 
-//This is the administrator class/
+//This is the administrator class
+
 
 public class Administrator extends User{
     public Administrator(String username, String email, String role, String id){
