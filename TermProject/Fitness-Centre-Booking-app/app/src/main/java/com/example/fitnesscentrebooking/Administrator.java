76217d2396762577583c0ad6508fee812e14efd3
Omar Aly <omaraly@omars-air-2.lan>
2021-10-20 20:18:04 -0400

    comment

--- TermProject/Fitness-Centre-Booking-app/app/src/main/java/com/example/fitnesscentrebooking/Administrator.java
+++ TermProject/Fitness-Centre-Booking-app/app/src/main/java/com/example/fitnesscentrebooking/Administrator.java
@@ -1,5 +1,7 @@
 package com.example.fitnesscentrebooking;
 
+//This is the administrator class
+
 public class Administrator extends User{
     public Administrator(String username, String email, String role, String id){
         super(username, email, role, id);
