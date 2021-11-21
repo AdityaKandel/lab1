b28ab50fdd9293e236e0f0e3a27279e80b47fcbc
rajbadial <rajvir.badial@gmail.com>
2021-11-21 13:29:28 -0500

    new test cases for courses

---
+++ TermProject/Fitness-Centre-Booking-app/app/src/androidTest/java/com/example/fitnesscentrebooking/CourseTest.java
@@ -0,0 +1,39 @@
+package com.example.fitnesscentrebooking;
+
+import junit.framework.TestCase;
+
+public class CourseTest extends TestCase {
+
+    public void testGetUserName() {
+        Course newCourse = new Course("Basketball", "11/21/2021", "2:30", "3", 25, "user name", "50");
+        assertEquals(newCourse.getUserName(), "user name");
+    }
+
+    public void testSetUserName() {
+        Course newCourse = new Course("Basketball", "11/21/2021", "2:30", "3", 25, "user name", "50");
+        newCourse.setUserName("meow");
+        assertEquals(newCourse.getUserName(), "meow");
+    }
+
+    public void testGetDate() {
+        Course newCourse = new Course("Basketball", "11/21/2021", "2:30", "3", 25, "user name", "50");
+        assertEquals(newCourse.getDate(), "11/21/2021");
+    }
+
+    public void testSetDate() {
+        Course newCourse = new Course("Basketball", "11/21/2021", "2:30", "3", 25, "user name", "50");
+        newCourse.setDate("12/25/2021");
+        assertEquals(newCourse.getDate(), "12/25/2021");
+    }
+
+    public void testTestGetName() {
+        Course newCourse = new Course("Basketball", "11/21/2021", "2:30", "3", 25, "user name", "50");
+        assertEquals(newCourse.getName(), "Basketball");
+    }
+
+    public void testTestSetName() {
+        Course newCourse = new Course("Basketball", "11/21/2021", "2:30", "3", 25, "user name", "50");
+        newCourse.setName("baloncesto");
+        assertEquals(newCourse.getName(), "baloncesto");
+    }
+}
